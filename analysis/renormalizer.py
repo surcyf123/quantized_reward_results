0cf90c69d955686cd5727457df2cf30f1b3af31b
Cole Miller <surcyf@gmail.com>
2023-09-11 08:25:30 -0400

    added total reward for pass mask row

--- analysis/renormalizer.py
+++ analysis/renormalizer.py
@@ -22,14 +22,6 @@ def process_file(filepath):
         "dpo_mean": (-11.78, 4.36)
     }
 
-def process_file(filepath):
-    # Read the file
-
-    mean_var_sets = {
-        "rlhf_mean": (0.75, 1.69),
-        "reciprocate_reward_mean": (2.91, 13.35),
-        "dpo_mean": (-11.78, 4.36)
-    }
     with open(filepath, 'r') as f:
         lines = f.readlines()
 
@@ -37,38 +29,41 @@ def process_file(filepath):
     if len(lines) <= 2 and any("gpu_name" in line for line in lines) and any("mean_duration" in line for line in lines):
         return
 
-    # Ensure that the rest of the code in the function is indented correctly
+    norm_values = {}
+    relevance_pass_rate = 1.0  # Default to 1 if not found
     for i, line in enumerate(lines):
+        if "relevance_pass_rate" in line:
+            relevance_pass_rate = float(line.split()[-1])
+            continue
+        for raw_str, (mean, var) in mean_var_sets.items():
+            if raw_str in line:
+                raw_value = float(line.split()[-1])
+                normalizer = RewardNormalizer(mean, var)
+                norm_value = normalizer.normalize_rewards(raw_value)
+                norm_values[raw_str] = norm_value
+                # Check if there is a next line and "_norm" is in it
+                if i < len(lines) - 1 and "_norm" in lines[i+1]:
+                    lines[i+1] = lines[i+1].split()[0] + " " + str(norm_value) + "\n"
 
-        norm_values = {}
-        relevance_pass_rate = 1.0  # Default to 1 if not found
-        for i, line in enumerate(lines):
-            if "relevance_pass_rate" in line:
-                relevance_pass_rate = float(line.split()[-1])
-            for raw_str, (mean, var) in mean_var_sets.items():
-                if raw_str in line:
-                    raw_value = float(line.split()[-1])
-                    normalizer = RewardNormalizer(mean, var)
-                    norm_value = normalizer.normalize_rewards(raw_value)
-                    norm_values[raw_str] = norm_value
-                    # Check if there is a next line and "_norm" is in it
-                    if i < len(lines) - 1 and "_norm" in lines[i+1]:
-                        lines[i+1] = lines[i+1].split()[0] + " " + str(norm_value) + "\n"
-
-
-    # Compute total_reward
+    # Compute total_reward and total_reward_excluding_relevance
     total_reward = (norm_values["rlhf_mean"] * 0.4 + norm_values["reciprocate_reward_mean"] * 0.3 + norm_values["dpo_mean"] * 0.3) * relevance_pass_rate
+    total_reward_excluding_relevance = (norm_values["rlhf_mean"] * 0.4 + norm_values["reciprocate_reward_mean"] * 0.3 + norm_values["dpo_mean"] * 0.3)
+    
     total_reward_str = f"total_reward {total_reward}\n"
+    total_reward_excluding_relevance_str = f"total_reward_excluding_relevance {total_reward_excluding_relevance}\n"
 
-    # Insert total_reward into the file
-    lines.append(total_reward_str)
+    # Check if the total_reward or total_reward_excluding_relevance lines are already present
+    if not any("total_reward " in line for line in lines):
+        lines.append(total_reward_str)
+    if not any("total_reward_excluding_relevance " in line for line in lines):
+        lines.append(total_reward_excluding_relevance_str)
 
     # Write back to the file
     with open(filepath, 'w') as f:
         f.writelines(lines)
 
 # Iterate over all .txt files in the parent directory
-parent_directory = "."
+parent_directory = "../"
 for filename in os.listdir(parent_directory):
     if filename.endswith(".txt") and filename != "normalizer.py":  # Exclude the script itself
         filepath = os.path.join(parent_directory, filename)
